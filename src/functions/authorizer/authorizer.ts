afa0e2cf18b7ab1c47736147b00fa099520f7e39
Wander Sanchez <56240295+Wander95@users.noreply.github.com>
2024-04-21 22:57:20 -0400

    feat(authorizer): add logic for forbidden

--- src/functions/authorizer/authorizer.ts
+++ src/functions/authorizer/authorizer.ts
@@ -1,22 +1,45 @@
 import { APIGatewayAuthorizerResult, APIGatewayTokenAuthorizerEvent } from 'aws-lambda';
+import 'dotenv/config';
 
-export const handler = async (event: APIGatewayTokenAuthorizerEvent): Promise<APIGatewayAuthorizerResult> => {
-    // TODO: Implement custom authorizer logic here
-    const token = event.authorizationToken;
-    const methodArn = event.methodArn;
-
-    // Return an authorization response indicating whether the request is authorized
-    return {
-        principalId: 'user',
-        policyDocument: {
-            Version: '2012-10-17',
-            Statement: [
-                {
-                    Action: 'execute-api:Invoke',
-                    Effect: 'Allow',
-                    Resource: methodArn,
-                },
-            ],
+const generateForbiddenResponse = (event: APIGatewayTokenAuthorizerEvent) => {
+  return {
+    principalId: 'user',
+    policyDocument: {
+      Version: '2012-10-17',
+      Statement: [
+        {
+          Action: 'execute-api:Invoke',
+          Effect: 'Deny',
+          Resource: event.methodArn,
         },
+      ],
+    },
+  };
+};
+
+export const handler = async (
+  event: APIGatewayTokenAuthorizerEvent
+): Promise<APIGatewayAuthorizerResult> => {
+  try {
+    const authHeader = event.authorizationToken;
+    const methodArn = event.methodArn;
+    const token = authHeader.split(' ')[1]!;
+    if (token !== process.env.AUTHORIZATION_TOKEN) return generateForbiddenResponse(event);
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: methodArn,
+          },
+        ],
+      },
     };
+  } catch (error) {
+    return generateForbiddenResponse(event);
+  }
 };
